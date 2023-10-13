b78623852d29ae6b556653a50d8223330cd6c128
fatmambo33 <fatmambo33@gmail.com>
2023-10-13 12:36:37 +0200

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='MatplotLibAPI',
-    version='1.1',
+    version='v2.0.0',
     packages=find_packages(),
     install_requires=[
         "pandas",
