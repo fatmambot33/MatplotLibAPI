7a7b8ed49d9f531f07bd3a93849585ec749e796a
fatmambo33 <fatmambo33@gmail.com>
2024-05-29 10:57:37 +0200

    modified:   MatplotLibAPI/__init__.py

--- MatplotLibAPI/__init__.py
+++ MatplotLibAPI/__init__.py
@@ -1,8 +1,5 @@
 
 
-
-
-
 from .Table import plot_table
 from .Timeserie import plot_timeserie
 from .Bubble import plot_bubble
@@ -10,6 +7,7 @@ from .Network import plot_network
 from .Pivot import plot_pivotbar
 from .Composite import plot_composite_bubble
 from .pdAccessor import MatPlotLibAccessor
+from .Style import StyleTemplate
 
-__all__ = ["plot_bubble","plot_timeserie","plot_table","plot_network","plot_pivotbar","plot_composite_bubble","MatPlotLibAccessor"]
-
+__all__ = ["plot_bubble", "plot_timeserie", "plot_table", "plot_network",
+           "plot_pivotbar", "plot_composite_bubble", "StyleTemplate", "MatPlotLibAccessor"]
