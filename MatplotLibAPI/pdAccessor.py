b6a38ad43c042cb52dec4ef4cd03c9988736be28
fatmambo33 <fatmambo33@gmail.com>
2024-05-29 12:31:01 +0200

    modified:   MatplotLibAPI/pdAccessor.py

--- MatplotLibAPI/pdAccessor.py
+++ MatplotLibAPI/pdAccessor.py
@@ -8,7 +8,7 @@ import pandas as pd
 from .Style import StyleTemplate
 from .Bubble import plot_bubble, BUBBLE_STYLE_TEMPLATE
 from .Composite import plot_composite_bubble
-from .Timeserie import plot_timeserie,TIMESERIE_STYLE_TEMPLATE
+from .Timeserie import plot_timeserie, TIMESERIE_STYLE_TEMPLATE
 from .Table import plot_table, TABLE_STYLE_TEMPLATE
 from .Network import (Graph)
 
@@ -88,24 +88,24 @@ class MatPlotLibAccessor:
                           ascending=ascending)
 
     def plot_timeserie(self,
-                  label: str,
-                  x: str,
-                  y: str,
-                  title: Optional[str] = None,
-                  style: StyleTemplate = TIMESERIE_STYLE_TEMPLATE,
-                  max_values: int = 20,
-                  sort_by: Optional[str] = None,
-                  ascending: bool = False) -> Axes:
+                       label: str,
+                       x: str,
+                       y: str,
+                       title: Optional[str] = None,
+                       style: StyleTemplate = TIMESERIE_STYLE_TEMPLATE,
+                       max_values: int = 20,
+                       sort_by: Optional[str] = None,
+                       ascending: bool = False) -> Axes:
 
         return plot_timeserie(pd_df=self._obj,
-                         label=label,
-                         x=x,
-                         y=y,
-                         title=title,
-                         style=style,
-                         max_values=max_values,
-                          sort_by=sort_by,
-                          ascending=ascending)
+                              label=label,
+                              x=x,
+                              y=y,
+                              title=title,
+                              style=style,
+                              max_values=max_values,
+                              sort_by=sort_by,
+                              ascending=ascending)
 
     def plot_network(self,
                      source: str = "source",
